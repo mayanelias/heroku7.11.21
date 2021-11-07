00bfa0b5f38f0620fda6f8bef2c6d45ba6c19c99
mayanelias <eliasmayan@gmail.com>
2021-11-07 11:12:15 +0200

    heroku

--- heroku/server.js
+++ heroku/server.js
@@ -2,7 +2,7 @@ const express = require("express"),
   app = express(),
   PORT = 8080;
   app.get("*", (req, res) => {
-    res.send("welcome");
+    res.send(`<h1>welcome!!!</h1>`);
   });
   app.listen(PORT, () => {
     console.log(`app is listening on: ${PORT}`);
